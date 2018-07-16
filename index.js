848337b9d027138a52f4b3e1e25c8ae1699448a2
LBWright <lwright.js@gmail.com>
2018-07-15 20:36:01 -0500

    added body parser and initialized stripe billing route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 const passport = require('passport')
 const keys = require('./config/keys')
 require('./models/User')
@@ -14,6 +15,9 @@ mongoose
   .then(console.log('connected to mongodb'))
 
 const app = express()
+
+app.use(bodyParser.json())
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
@@ -24,6 +28,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
